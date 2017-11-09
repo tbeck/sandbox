2f19e3e03a8ae8b9a5fa7db850d225650cf788b6
Tom Beck <tom@tastylounge.com>
2017-11-09 11:59:44 -0800

    removed typeahead logic from main js

--- src/_assets/scripts/main.js
+++ src/_assets/scripts/main.js
@@ -1,5 +1,4 @@
 require('jquery');
-// require('typeahead');
 var search = require('./search');
 
 $(function() {
@@ -58,30 +57,4 @@ $(document).ready(function() {
     $('.confirm-age button').remove();
    });
 
-   
-
-   // Initialize typeahead feature
-   // var $input = $('#search');
-   // $input.typeahead({
-   //   source: [
-   //     {id: 'someId1', name: 'Captain Truman'},
-   //     {id: 'someId2', name: 'Valentino'},
-   //     {id: 'someId3', name: 'Habana Libre'}
-   //   ],
-   //   autoSelect: true
-   // });
-   // $input.change(function() {
-   //   var current = $input.typeahead("getActive");
-   //   if (current) {
-   //     // Some item from your model is active!
-   //     if (current.name == $input.val()) {
-   //       // This means the exact match is found. Use toLowerCase() if you want case insensitive match.
-   //     } else {
-   //       // This means it is only a partial match, you can either add a new item
-   //       // or take the active if you don't want new items
-   //     }
-   //   } else {
-   //     // Nothing is active so it is a new value (or maybe empty value)
-   //   }
-   // });
 });
--- src/_assets/scripts/search.js
+++ src/_assets/scripts/search.js
@@ -1,5 +1,7 @@
+
 var mixitup = require('mixitup');
 var Recipes = require('./recipes');
+// require('typeahead');
 
 
 var container = document.querySelector('[data-ref="container"]');
@@ -87,6 +89,31 @@ if(inputSearch) {
   });
 }
 
+
+// Initialize typeahead feature
+// var $input = $('#search');
+// $input.typeahead({
+//   source: [
+//     {id: 'someId1', name: 'Captain Truman'},
+//     {id: 'someId2', name: 'Valentino'},
+//     {id: 'someId3', name: 'Habana Libre'}
+//   ],
+//   autoSelect: true
+// });
+// $input.change(function() {
+//   var current = $input.typeahead("getActive");
+//   if (current) {
+//     // Some item from your model is active!
+//     if (current.name == $input.val()) {
+//       // This means the exact match is found. Use toLowerCase() if you want case insensitive match.
+//     } else {
+//       // This means it is only a partial match, you can either add a new item
+//       // or take the active if you don't want new items
+//     }
+//   } else {
+//     // Nothing is active so it is a new value (or maybe empty value)
+//   }
+// });
 
 filterByString = function(searchValue) {
   if (searchValue) {
