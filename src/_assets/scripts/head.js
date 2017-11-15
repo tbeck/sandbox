d67704eb2ff0988376580ae9cf5391bc3a4731c2
Tom Beck <tom@tastylounge.com>
2017-11-15 03:21:08 -0800

    removing FOUT. Needs refactor.

--- src/_assets/scripts/head.js
+++ src/_assets/scripts/head.js
@@ -10,11 +10,11 @@ WebFont.load({
   },
   active: function() {
     console.log('FONTS ARE ACTIVE');
-    $('#mainnav .navbar-nav, .hero-caption').addClass('animated fadeIn');
+    // $('#mainnav .navbar-nav, .hero-caption').addClass('animated fadeIn');
   },
   inactive: function() {
     console.log('FONTS ARE NOT ACTIVE');
-    $('#mainnav .navbar-nav, .hero-caption').addClass('animated fadeIn');
+    // $('#mainnav .navbar-nav, .hero-caption').addClass('animated fadeIn');
   },
   fontloading: function(familyName, fvd) {},
   fontactive: function(familyName, fvd) {},
