37717326e88e68f6fa18a365e96a80936b579eb5
Jon Burr <yojonathanwburr@gmail.com>
2018-12-19 11:52:16 -0800

    Event filtering

--- src/_assets/scripts/main.js
+++ src/_assets/scripts/main.js
@@ -1,61 +1,71 @@
-require('jquery');
-require('bootstrap');
-require('./search');
-var Cookies = require('js-cookie');
+require("jquery");
+require("bootstrap");
+require("./search");
+var Cookies = require("js-cookie");
 
-$(window).scroll(function(){
+$(window).scroll(function() {
   if ($(window).scrollTop() >= 250) {
-    $('.homepage-layout #page-header').removeClass('home-header');
-   }
-   else {
-    $('.homepage-layout #page-header').addClass('home-header');
-   }
+    $(".homepage-layout #page-header").removeClass("home-header");
+  } else {
+    $(".homepage-layout #page-header").addClass("home-header");
+  }
 });
 
 if ($(window).scrollTop() >= 250) {
-  $('.homepage-layout #page-header').removeClass('home-header');
+  $(".homepage-layout #page-header").removeClass("home-header");
 } else {
-  $('.homepage-layout #page-header').addClass('home-header');
+  $(".homepage-layout #page-header").addClass("home-header");
 }
 
-
 $(document).ready(function() {
-   // nav interaction
-   $('.navbar-toggler').on('click', function(){
-     $(this).toggleClass('open');
-   });
+  // nav interaction
+  $(".navbar-toggler").on("click", function() {
+    $(this).toggleClass("open");
+  });
 
-   //age verification
-   function setCookie() {
-      Cookies.set('age', 'verified');
-   }
+  //age verification
+  function setCookie() {
+    Cookies.set("age", "verified");
+  }
 
-   function checkCookie() {
-      var age = Cookies.get('age');
-      if(age === 'verified') {
-        // window.console.log('age is', age);
-        $('#age-verification').remove();
+  function checkCookie() {
+    var age = Cookies.get("age");
+    if (age === "verified") {
+      // window.console.log('age is', age);
+      $("#age-verification").remove();
+    } else {
+      // window.console.log('age is not verified');
+      $("#age-verification").addClass("active");
+    }
+  }
 
-      }else {
-        // window.console.log('age is not verified');
-        $('#age-verification').addClass('active');
-      }
-   }
+  checkCookie();
 
-   checkCookie();
-
-   $('#yes').on('click', function(){
+  $("#yes").on("click", function() {
     setCookie();
     checkCookie();
-   });
+  });
 
-   $('#no').on('click', function(){
-    window.console.log('no clicked');
-    $('.age-message').text('Sorry! Our website is only available to users who are of legal drinking age.');
-    $('.confirm-age button').remove();
-   });
+  $("#no").on("click", function() {
+    window.console.log("no clicked");
+    $(".age-message").text(
+      "Sorry! Our website is only available to users who are of legal drinking age."
+    );
+    $(".confirm-age button").remove();
+  });
 
-   $('.venobox').venobox();
+  $(".venobox").venobox();
 
-  
+  var mixer = mixitup(".events-row", {
+    controls: {
+      toggleLogic: "none"
+    }
+  });
+
+  $(function() {
+    var filterSelect = $("#event-filter-select");
+    filterSelect.on("change", function() {
+      mixer.filter(this.value);
+    });
+  });
 });
