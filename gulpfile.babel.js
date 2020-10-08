ae7cdb0c2599b46420033cbc685c45916ba498ad
tombeck <t@ocupop.com>
2020-10-08 08:50:14 -0700

    refactor build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,38 +1,9 @@
-const settings = {
-  // scripts: true,		// Turn on/off script tasks
-  // polyfills: true,	// Turn on/off polyfill tasks
-  // styles: false,		// Turn on/off style tasks
-  // svgs: false,		// Turn on/off SVG tasks
-  // static: false,		// Turn on/off static file copying
-  // deploy: true,		// Turn on/off all deployment tasks
-  // cacheBust: false,	// Turn on/off cache busting (adds a version number to minified files)
-};
-const pkg = require("./package.json");
-const gulp = require("gulp");
-const suite = require("@cloudcannon/suite");
-const { buildScripts } = require("./tasks/webpack");
-const webpack = require("webpack");
-const webpackDevMiddleware = require("webpack-dev-middleware");
-const webpackHotMiddleware = require("webpack-hot-middleware");
+const gulp = require("gulp")
+const suite = require('@cloudcannon/suite')
+const { scripts } = require('./tasks/scripts')
 
-const { config } = require("./tasks/webpack");
-const bundler = webpack(config);
+suite.dev(gulp)
 
-suite.dev(gulp, {
-  serve: {
-    middleware: [
-      webpackDevMiddleware(bundler, {
-        /* options */
-      }),
-      webpackHotMiddleware(bundler)
-    ]
-  }
-});
+gulp.task('default', gulp.parallel('scripts', 'dev'));
 
-// function hello(cb) {
-//   console.log('hello from gulp');
-//   cb();
-// }
-// exports.hello = hello;
-
-exports.buildScripts = buildScripts;
+exports.scripts = scripts
