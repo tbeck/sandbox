f9decb068e062da9dbeb36b19ab3c8db7b995945
tombeck <t@ocupop.com>
2022-06-21 14:18:59 -0700

    convert to static site

--- gulpfile.babel.js
+++
@@ -1,9 +0,0 @@
-const gulp = require("gulp")
-const suite = require('@cloudcannon/suite')
-const { scripts } = require('./tasks/scripts')
-
-suite.dev(gulp)
-
-gulp.task('default', gulp.parallel('scripts', 'dev'));
-
-exports.scripts = scripts
